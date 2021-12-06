8563ea3bbac055911d2af8ddff5876dfef6c2592
Mark <mark.neves@gmail.com>
2021-12-06 17:20:14 +0000

    Question 6 part 2

---
+++ 2021/06/06-part2.ps1
@@ -0,0 +1,31 @@
+$ErrorActionPreference="Stop"
+$data=@()
+Get-Content "$PSScriptRoot\06.txt" | %{$data+=$_}
+
+$fish=@{}
+$numFish=0
+$data.Split(',') | %{$fish[$numFish++]=[int]$_}
+$fish.Values -join ','
+
+$numDays=256
+
+$days=@{}
+for ($i=0;$i -lt $numFish;$i++)
+{
+    for ($d=$fish[$i]+1;$d -le $numDays;$d+=7)
+    {
+        $days[$d]+=1
+    }
+}
+
+for ($d=1;$d -le $numDays;$d++)
+{
+    for ($i=$d+9;$i -le $numDays;$i+=7)
+    {
+        $days[$i] += $days[$d]
+    }
+}
+$total=($days.Values |  measure -sum).Sum + $numFish
+$total
+
+#for ($i=1;$i -le $numDays;$i++) { "Day $i : $($days[$i])"}
