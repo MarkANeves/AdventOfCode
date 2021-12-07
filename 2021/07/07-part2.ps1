6302b95a3e3c0f9b0fcccd3347dfcff5d675a9d5
Mark <mark.neves@gmail.com>
2021-12-07 12:00:27 +0000

    Question 7 part 2 using formula

--- 2021/07/07-part2.ps1
+++ 2021/07/07-part2.ps1
@@ -8,25 +8,12 @@ $data.Split(',') | %{$crabPos+=[int]$_}
 $minPos = ($crabPos | measure -Minimum).Minimum
 $maxPos = ($crabPos | measure -Maximum).Maximum
 
-function Cost($d)
-{
-    $cost=0
-    1..$d | %{$cost+=$_}
-    return $cost
-}
-
-$costLookup=@()
-$costLookup+=0
-1..$maxPos | %{ $costLookup += Cost $_}
-
-"$($stopwatch.Elapsed.hours):$($stopwatch.Elapsed.minutes):$($stopwatch.Elapsed.seconds):$($stopwatch.Elapsed.Milliseconds)"
-
 $minCost=[int64]::MaxValue
 $cheapestPos=$maxPos+1
 for ($p=$minPos;$p -le $maxPos;$p++)
 {
     $cost=0
-    $crabPos | %{ $cost+= $costLookup[[Math]::Abs($_ - $p)] }
+    $crabPos | %{ $d=[Math]::Abs($_ - $p);$cost+= ($d*($d+1))/2}
     if ($cost -lt $minCost)
     {
         $minCost=$cost
